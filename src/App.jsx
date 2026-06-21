8bedb0a28425ef87a1d71942216aedf303bce3a8
Jason Feimster <jasonfeimster@gmail.com>
2026-06-20 22:36:32 -0400

    Create App component with ReserveUploadDashboard

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReserveUploadDashboard from './components/ReserveUploadDashboard';
+
+export default function App() {
+  return (
+    <div className="w-full">
+      <ReserveUploadDashboard />
+    </div>
+  );
+}
